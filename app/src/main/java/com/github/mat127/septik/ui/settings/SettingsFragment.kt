18f1f7510ebec3724e9bc49df73ac60c6610bc04
Petr Matoušek <petr.matousek@comprimato.com>
2023-04-07 23:59:20 +0200

    [feat] limit preferences values to numbers

--- app/src/main/java/com/github/mat127/septik/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/github/mat127/septik/ui/settings/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package com.github.mat127.septik.ui.settings
 
 import android.os.Bundle
+import android.text.InputType
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.github.mat127.septik.R
 
@@ -8,5 +10,14 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        findPreference<EditTextPreference>("volume")?.let(::numberOnly)
+        findPreference<EditTextPreference>("water_price")?.let(::numberOnly)
+        findPreference<EditTextPreference>("empting_price")?.let(::numberOnly)
+    }
+
+    private fun numberOnly(preference: EditTextPreference) {
+        preference.setOnBindEditTextListener {
+            it.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+        }
     }
 }
